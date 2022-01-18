c0086cb800c53e2121d5a68e65dfe84538d7ccf7
Devin Jaggernauth <neexis@gmail.com>
2022-01-17 22:55:22 -0400

    create user route

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
-const { PORT = 3000} = process.env;
+mongoose.connect('mongodb://localhost:27017/aroundb');
+app.use(express.json());
 
-app.listen(PORT, ()=>{
-  console.log(`Server is running on ${PORT}`)
-})
+const { PORT = 3000 } = process.env;
+const userRouter = require('./routes/users');
+const articleRouter = require('./routes/articles')
+
+app.use('/cards', articleRouter);
+
+app.use('/users', userRouter);
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server is running on ${PORT}`);
+});
---
+++ routes/users.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const router = express.Router();
+
+const getCurrentUser = require('../controllers/users');
+
+router.get('/me', getCurrentUser);
